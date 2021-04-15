def6a797bd876dae1dd005f5770d258b4075caee
madzisko1995 <madzisko1995@gmail.com>
2021-04-15 17:08:06 +0200

    /method

--- main.py
+++ main.py
@@ -23,3 +23,24 @@ def counter():
 @app.get("/hello/{name}", response_model=HelloResp)
 async def read_item(name: str):
     return HelloResp(msg=f"Hello {name}")
+
+
+@app.get("/method", status_code=200)
+def give_method():
+    return {"method": "GET"}
+
+@app.put("/method", status_code=200)
+def give_method():
+    return {"method": "PUT"}
+
+@app.options("/method", status_code=200)
+def give_method():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method", status_code=200)
+def give_method():
+    return {"method": "DELETE"}
+
+@app.post("/method", status_code=201)
+def give_method():
+    return {"method": "POST"}
--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello world!"}
 
 
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
@@ -26,3 +26,25 @@ def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
     assert response.text == '"2"'
+
+
+def test_methods():
+    response = client.get(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+    response = client.post(f"/method")
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
+
+    response = client.put(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+    response = client.options(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "OPTIONS"}
+
+    response = client.delete(f"/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
