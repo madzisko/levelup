75b4aed465864c095e2aa7ec083cab6f051830e5
madzisko1995 <madzisko1995@gmail.com>
2021-05-17 13:27:48 +0200

    structure change

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("postgresgl://ukhnjxttipiuta:9992dea7c69d2c60880bdcd604b777a2fcd3afcb91ae25d6cd1528de83992fe6@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/dcbfvqtcomehuu")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
